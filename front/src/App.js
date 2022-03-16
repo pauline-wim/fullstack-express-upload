6c7474bb05ebf79ce9e2cf046f0cb4ab7e89d6ce
pauline-wim <linette.wim@hotmail.com>
2022-03-16 17:44:50 +0100

    Working on list of users

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import "./App.css";
 
 function App() {
   const [image, setImage] = useState("");
+  const [username, setUsername] = useState("");
+  // const [userList, setUserList] = useState([]);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const formData = new FormData();
-  // const fileField = document.querySelector('input[type="file"]');
+  const fileField = useRef();
 
-  // formData.append("img", fileField.files[0]);
+  // useEffect(() => {
+  //   fetch("/users")
+  //     .then((res) => res.json(res))
+  //     .then((res) => {
+  //       console.log(res);
+  //       setUserList(res);
+  //     });
+  // }, []);
 
   const onSubmit = (data) => {
-    console.log(data.img[0].name);
-    setImage(data.img[0].name);
-    fetch("http://localhost:8000/user", {
+    setUsername(data.username);
+    // console.log(data.username);
+    const formData = new FormData();
+    formData.append("img", fileField.current.files[0]);
+    formData.append("username", data.username);
+    console.log(fileField.current.files[0].name);
+    setImage(fileField.current.files[0].name);
+    fetch("/user", {
       method: "POST",
-      // headers: {
-      //   "Content-Type": "multipart/form-data",
-      // },
       body: formData,
     });
   };
@@ -35,10 +45,11 @@ function App() {
           placeholder="Username"
           {...register("username", { required: true })}
         />
-        <input {...register("img")} type="file" />
+        <input {...register("img")} type="file" ref={fileField} />
         <input className="btn" type="submit" value="Send" />
       </form>
-      {/* {image ? <img src={image} alt="image" /> : null} */}
+      {/* {userList ? <div>{userList}</div> : null} */}
+      {/* {image ? <img src={`${image}`} alt="image" /> : null} */}
     </main>
   );
 }
