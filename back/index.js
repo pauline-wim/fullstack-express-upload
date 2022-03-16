bc91fd5c4d22419415e8c115ec14658765604a26
pauline-wim <linette.wim@hotmail.com>
2022-03-16 17:46:13 +0100

    Created userList -> users gathered from front form

--- back/index.js
+++ back/index.js
@@ -6,17 +6,25 @@ const path = require("path");
 const app = express();
 const upload = multer({ dest: "public/uploads" });
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.static("public"));
+
+const userList = [];
 
 app.post("/user", upload.single("img"), (req, res) => {
   fs.renameSync(
     req.file.path,
     path.join(req.file.destination, req.file.originalname)
   );
-
-  res.send("Image received");
+  userList.push(req.body.username);
+  res.json(userList);
+  //   console.log(req.body.username);
+  //   res.send("Image received");
 });
+
+// app.get("/user", (req, res) => {
+//   res.json(userList);
+// });
 
 app.listen(8000, () => {
   console.log("Listening on port 8000");
