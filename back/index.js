cae1b03a144b52a11ac202d68b9b0e719c142cbb
pauline-wim <linette.wim@hotmail.com>
2022-03-16 14:59:58 +0100

    upload image to public/uploads

--- back/index.js
+++ back/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const app = express();
-const upload = multer({ dest: "public" });
+const upload = multer({ dest: "public/uploads" });
 
 app.use(express.static("public"));
+
+app.post("/user", upload.single("img"), (req, res) => {
+  fs.renameSync(
+    req.file.path,
+    path.join(req.file.destination, req.file.originalname)
+  );
+
+  res.send("Image received");
+});
 
 app.listen(8000, () => {
   console.log("Listening on port 8000");
