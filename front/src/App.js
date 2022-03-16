e22f8683a27091204907d04b441241e7b0905359
pauline-wim <linette.wim@hotmail.com>
2022-03-16 16:17:45 +0100

    Link front to back

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const formData = new FormData();
+  // const fileField = document.querySelector('input[type="file"]');
+
+  // formData.append("img", fileField.files[0]);
+
+  const onSubmit = (data) => {
+    console.log(data.img[0].name);
+    setImage(data.img[0].name);
+    fetch("http://localhost:8000/user", {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "multipart/form-data",
+      // },
+      body: formData,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>WELCOME</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Username"
+          {...register("username", { required: true })}
+        />
+        <input {...register("img")} type="file" />
+        <input className="btn" type="submit" value="Send" />
+      </form>
+      {/* {image ? <img src={image} alt="image" /> : null} */}
+    </main>
   );
 }
 
